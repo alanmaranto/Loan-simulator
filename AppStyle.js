c6935246544b9be783145049344b627e15525383
alan <alanarostegui@hotmail.com>
2020-09-14 23:58:15 -0500

    feature(app): structure

---
+++ AppStyle.js
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+
+export const AppStyles = StyleSheet.create({
+  safeArea: {
+    backgroundColor: '#f00',
+    height: 200,
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+    alignItems: 'center',
+  },
+});
